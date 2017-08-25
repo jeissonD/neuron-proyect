9c9613d9ae5ab008768bd49cf51e3b1e3672d3d5
Jeisson Lazo <corsario_ll@hotmail.com>
2017-08-25 17:10:27 -0500

    practic changes

---
+++ web/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<header>
+		<h1>{{title}}</h1>
+	</header>
+</template>
+
+<script>
+	
+export default {
+  name: 'app',
+  data () {
+    return {
+			title : 'title'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+	h1{
+		color: black;
+	}
+</style>
